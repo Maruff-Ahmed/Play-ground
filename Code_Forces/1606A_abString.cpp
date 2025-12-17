e7bd3285471d6f9bcc146118019f5a58cc751389
Maruf <marufahmmed208@gmail.com>
2025-12-17 23:07:27 +0600

    solved : string palindrome

---
+++ Code_Forces/1606A_abString.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve(){
+
+
+    string s;
+    cin>>s;
+
+    if(s[0]!=s[s.size()-1]){
+        s[0]=s[s.size()-1];
+
+    }
+    cout<<s<<endl;
+
+
+		
+}
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
