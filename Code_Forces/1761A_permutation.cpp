fb8fbeff56e54bb96cd50f89eeca77d04c6e4b1c
Maruf <marufahmmed208@gmail.com>
2025-12-19 23:05:20 +0600

    solved : permutation easy problem

---
+++ Code_Forces/1761A_permutation.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve(){
+
+
+   long long n, a, b;
+    cin >> n >> a >> b;
+              
+    if (a + b + 2 <= n || (a == b && a == n)){
+         cout << "Yes" << endl; 
+    }
+    else{
+        cout << "No" << endl;
+    }
+		
+}
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
