42731fed9e0bf3b2d8d0820cf2f2d55ed76e0bba
Maruf <marufahmmed208@gmail.com>
2025-12-24 22:05:03 +0600

    solved : square year problem

--- Code_Forces/2114_A.cpp
+++ Code_Forces/2114_A.cpp
@@ -2,36 +2,17 @@
 using namespace std;
 
 void solve() {
-    int n;
-    cin >> n;
+    string s;
+    cin>>s;
+    long long n= stoi(s);
+    int r= sqrt(n);
 
-    vector<int> v(n);
-    for (int i = 0; i < n; i++) {
-        cin >> v[i];
+    if(r*r==n){
+        cout<<0<< " "<<r<<endl;
+    }else{
+         cout<<-1<<endl;
     }
-
-   
-    long long total = 0;
-    for (int i = 0; i + 1 < n; i++) {
-        total += abs(v[i] - v[i + 1]);
-    }
-
-   
-    long long time = 0;
-    time = max(time, (long long)abs(v[0] - v[1]));
-
     
-    time = max(time, (long long)abs(v[n - 2] - v[n - 1]));
-
-    for (int i = 1; i + 1 < n; i++) { 
-        long long x = abs(v[i - 1] - v[i]) + abs(v[i] - v[i + 1]);
-
-        long long y = abs(v[i - 1] - v[i + 1]);
-
-        time = max(time, x - y);
-    }
-
-    cout << total - time << endl;
 }
 
 int main() {
