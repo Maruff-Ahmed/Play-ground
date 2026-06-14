0b8adcb1f7e21950383ee7ec6e3c8286c70973fe
Maruf <marufahmmed208@gmail.com>
2026-06-14 20:21:18 +0600

    solved div 3 question A

---
+++ Code_Forces/2227_A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void solve() {
+   int x, y;
+    cin >> x>> y;
+
+    if(x%2==0){
+        cout<<"YES"<<endl;
+    }
+    else if(y%2==0){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+    
+}
+ 
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+}
